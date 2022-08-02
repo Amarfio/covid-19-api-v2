db4c446365ea4970e85ea4dff7e8e93e3e3788e8
= <=>
2022-08-02 04:00:03 +0000

    updated the CovidtestController with get all tests method.

--- src/main/java/com/work/covid19apiv2/controllers/CovidtestController.java
+++ src/main/java/com/work/covid19apiv2/controllers/CovidtestController.java
@@ -1,5 +1,53 @@
 package com.work.covid19apiv2.controllers;
 
+
+import com.work.covid19apiv2.EmailSenderService;
+import com.work.covid19apiv2.model.Covidtest;
+import com.work.covid19apiv2.service.CovidTestService;
+import com.work.covid19apiv2.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@RestController
+@RequestMapping("/covid-19/api/v2")
 public class CovidtestController {
+
+
+    //use this service to get test details for covid
+    public CovidTestService covidTestService;
+
+    //declaration to use the email service here
+    @Autowired
+    EmailSenderService senderService;
+
+
+//    public CovidtestController(UserService userService){
+//        this.userService = userService;
+//    }
+    public CovidtestController(CovidTestService covidTestService){this.covidTestService = covidTestService;}
+
+
+    @Operation(summary="Get covid tests", description = "Get a list of covid tests")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the tests",
+                    content = {@Content(mediaType = "application/json", schema = @Schema(implementation = Covidtest.class))}),
+            @ApiResponse(responseCode = "404", description = "Tests not found",
+                    content = @Content)})
+    @GetMapping("/covidtests/get-all-tests")
+    public List<Covidtest> getAllCovidTests() throws ExecutionException, InterruptedException {
+        return covidTestService.getAllTests();
+    }
+
+
 
 }
--- src/main/java/com/work/covid19apiv2/service/CovidTestService.java
+++ src/main/java/com/work/covid19apiv2/service/CovidTestService.java
@@ -1,4 +1,42 @@
 package com.work.covid19apiv2.service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.Firestore;
+import com.google.firebase.cloud.FirestoreClient;
+import com.work.covid19apiv2.model.Covidtest;
+import com.work.covid19apiv2.model.User;
+import org.springframework.stereotype.Service;
+
+import javax.swing.text.Document;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+@Service
 public class CovidTestService {
+
+    //method to get all tests done so far
+    public List<Covidtest> getAllTests() throws ExecutionException, InterruptedException {
+        Firestore dbFirestore = FirestoreClient.getFirestore();
+
+        Iterable<DocumentReference> documentReference = dbFirestore.collection("covidtests").listDocuments();
+        Iterator<DocumentReference> iterator = documentReference.iterator();
+
+        List<Covidtest> covidtestList = new ArrayList<>();
+        Covidtest covidtest = null;
+
+        while(iterator.hasNext()){
+            DocumentReference documentReference1 = iterator.next();
+            ApiFuture<DocumentSnapshot> future = documentReference1.get();
+            DocumentSnapshot document = future.get();
+
+            covidtest = document.toObject(Covidtest.class);
+            covidtestList.add(covidtest);
+        }
+
+        return covidtestList;
+    }
 }
